69109f4e7cb13ef2a1bc19e236d4ab044ae65af5
Thushanth <116872194+thushanth24@users.noreply.github.com>
2025-02-21 00:12:26 +0530

    backend and db connected

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import connectDB from './config/connectDB.js'
+
+
+const app = express()
+app.use(cors({
+    credentials : true,
+    origin : process.env.FRONTEND_URL
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan())
+app.use(helmet({
+    crossOriginResourcePolicy : false
+}))
+
+const PORT = 8080 || process.env.PORT 
+
+app.get("/",(request,response)=>{
+    ///server to client
+    response.json({
+        message : "Server is running " + PORT
+    })
+})
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is running",PORT)
+    })
+})
+
+
