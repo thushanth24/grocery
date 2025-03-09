2867e5e9bfadf65a68b3e7af668ec2bc67dd7bfa
Thushanth <116872194+thushanth24@users.noreply.github.com>
2025-03-09 21:53:07 +0530

    header,footer created

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import Search from './Search'
+
+function Header() {
+  return (
+    <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white'>
+       <div className='container mx-auto flex items-center px-2 justify-between'>
+               <div className='h-full'>
+                                    <div className='h-full flex justify-center items-center'>
+                                        <img 
+                                            src={logo}
+                                            width={170}
+                                            height={60}
+                                            alt='logo'
+                                            className='hidden lg:block'
+                                        />
+                                       
+                                    </div>
+
+                                    
+                </div>    
+                 {/**Search */}
+                 <div className='hidden lg:block'>
+                                    <Search/>
+                  </div>       
+
+                  {/**login and my cart */}
+                  <div className=''>
+                            login and my cart       
+                  </div>
+       </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/src/components/Search.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+import { TypeAnimation } from 'react-type-animation';
+
+
+function Search() {
+  return (
+    <div className='w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 '>
+      <div>
+            <button className='flex justify-center items-center h-full p-3 group-focus-within:text-primary-200'>
+                                <IoSearch size={22}/>
+            </button>
+      </div>
+
+     <div className='w-full h-full'>
+        <div  className='w-full h-full flex items-center'>
+
+            <TypeAnimation
+                                sequence={[
+                                    // Same substring at the start will only be typed out once, initially
+                                    'Search "milk"',
+                                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                    'Search "bread"',
+                                    1000,
+                                    'Search "sugar"',
+                                    1000,
+                                    'Search "panner"',
+                                    1000,
+                                    'Search "chocolate"',
+                                    1000,
+                                    'Search "curd"',
+                                    1000,
+                                    'Search "rice"',
+                                    1000,
+                                    'Search "egg"',
+                                    1000,
+                                    'Search "chips"',
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                repeat={Infinity}
+                            />
+                      </div>
+
+         </div>
+    </div>
+  )
+}
+
+export default Search
